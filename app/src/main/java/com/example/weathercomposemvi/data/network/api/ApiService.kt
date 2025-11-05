bbe4860767be51d74e542fb40430d51ee8c8a675
Clickonuser <clickonuser@gmail.com>
2025-11-05 20:05:42 +0300

    Add API key interceptor to Retrofit client

--- app/src/main/java/com/example/weathercomposemvi/data/network/api/ApiService.kt
+++ app/src/main/java/com/example/weathercomposemvi/data/network/api/ApiService.kt
@@ -9,18 +9,18 @@ import retrofit2.http.Query
 
 interface ApiService {
 
-    @GET("current.json?key=df1c637cf2e6440f982132240241907")
+    @GET("current.json")
     suspend fun loadCurrentWeather(
         @Query("q") query: String
     ): WeatherCurrentDto
 
-    @GET("forecast.json?key=df1c637cf2e6440f982132240241907")
+    @GET("forecast.json")
     suspend fun loadForecast(
         @Query("q") query: String,
         @Query("days") daysCount: Int = 4
     ): WeatherForecastDto
 
-    @GET("search.json?key=df1c637cf2e6440f982132240241907")
+    @GET("search.json")
     suspend fun searchCity(
         @Query("q") query: String,
     ): List<CityDto>
