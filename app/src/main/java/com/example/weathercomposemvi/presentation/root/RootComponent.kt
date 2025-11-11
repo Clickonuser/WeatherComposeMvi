952fca02335d611a354d380e850a26269c1977d1
Clickonuser <clickonuser@gmail.com>
2025-11-11 12:51:59 +0300

    RootComponent p.1

--- app/src/main/java/com/example/weathercomposemvi/presentation/root/RootComponent.kt
+++ app/src/main/java/com/example/weathercomposemvi/presentation/root/RootComponent.kt
@@ -1,4 +1,19 @@
 package com.example.weathercomposemvi.presentation.root
 
+import com.arkivanov.decompose.router.stack.ChildStack
+import com.arkivanov.decompose.value.Value
+import com.example.weathercomposemvi.presentation.details.DetailsComponent
+import com.example.weathercomposemvi.presentation.favourite.FavouriteComponent
+import com.example.weathercomposemvi.presentation.search.SearchComponent
+
 interface RootComponent {
+
+    val stack: Value<ChildStack<*, Child>>
+
+    sealed interface Child {
+
+        data class Favourite(val component: FavouriteComponent): Child
+        data class Search(val component: SearchComponent): Child
+        data class Details(val component: DetailsComponent): Child
+    }
 }
